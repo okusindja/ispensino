6c809ff8e83828a7003f697025ddfaa6d6daeb44
Okusindja de Almeida <okusindja@gmail.com>
2025-07-01 16:47:26 +0100

    :sparkles: feat: initial configurations

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,13 +1,14 @@
 import { getApp, getApps, initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: 'AIzaSyAE5SCxvX6JS5TSWXGqp8g5-muF_wj4LOo',
+  authDomain: 'ispensino.firebaseapp.com',
+  projectId: 'ispensino',
+  storageBucket: 'ispensino.firebasestorage.app',
+  messagingSenderId: '380756592347',
+  appId: '1:380756592347:web:23d9d4ad0c93d3ce126efe',
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
