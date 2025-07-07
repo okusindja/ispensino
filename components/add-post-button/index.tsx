29e99cd55a6f67d8737ad843cc7f570db0386bfa
Okusindja de Almeida <okusindja@gmail.com>
2025-07-07 17:44:40 +0100

    :construction: feat: wip

--- components/add-post-button/index.tsx
+++ components/add-post-button/index.tsx
@@ -5,13 +5,13 @@ import { PlusSVG } from '../svg';
 const AddPostButton = () => {
   return (
     <Button
-      variant="primary"
-      size="large"
       isIcon
-      position="fixed"
-      bottom="5rem"
       right="1rem"
+      size="large"
+      bottom="6rem"
       zIndex="99999"
+      position="fixed"
+      variant="primary"
     >
       <PlusSVG maxWidth="1.5rem" maxHeight="1.5rem" width="100%" />
     </Button>
